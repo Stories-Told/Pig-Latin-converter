d91e7cab5f154296f66eba76c8b4c8e7137de878
Stories-Told <54217603+Stories-Told@users.noreply.github.com>
2020-01-30 22:33:58 -0500

    Add files via upload

---
+++ Learning_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	enum Day
+	{
+			SUN, MON, TUE,
+			WED, THUR, FRI, 
+			SAT
+	};
+
+	printf(SUN);
+
+	return 0;
+}
